d3b85add619eec97e85f4a46bdc024721096bce0
Christos-Spyridon Karydis <115801950+chriskarydis@users.noreply.github.com>
2024-06-30 15:35:30 +0300

    Add files via upload

--- mysql_medicaldb/visit/visit.php
+++ mysql_medicaldb/visit/visit.php
@@ -27,13 +27,17 @@
             text-align: center;
             width: 90%;
             max-width: 800px;
+        }
+
+        .table-container {
+            width: 100%;
             overflow-x: auto;
+            margin-top: 20px;
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin-top: 20px;
         }
 
         table, th, td {
@@ -90,14 +94,16 @@
         <div class="table-container">
             <table>
                 <tr>
-                    <th>Visit ID</th><th>AMKA</th><th>Doctor ID</th><th>Hospital/Clinic ID</th><th>Visit Date</th><th>Visit Type</th><th>Edit</th><th>Delete</th>
+                    <th>Visit ID</th><th>AMKA</th><th>Doctor ID</th><th>Doctor Name</th><th>Hospital/Clinic ID</th><th>Hospital Name</th><th>Visit Date</th><th>Visit Type</th><th>Edit</th><th>Delete</th>
                 </tr>       
                 <?php        
                 include 'C:/xampp/htdocs/mysql_medicaldb/connDB.php';
 
-                $sql = "SELECT visitid, visit_date, visit_type, amka, doctorid, hospitalclinicid
-                        FROM visit
-                        ORDER BY visitid";
+                $sql = "SELECT v.visitid, v.amka, v.doctorid, d.firstname, d.lastname, v.hospitalclinicid, h.hosp_name, v.visit_date, v.visit_type
+                        FROM visit v
+                        JOIN doctor d ON v.doctorid = d.doctorid
+                        JOIN hospital_clinic h ON v.hospitalclinicid = h.hospitalclinicid
+                        ORDER BY v.visitid";
 
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
@@ -105,17 +111,19 @@
                         $visitid = htmlspecialchars($row["visitid"]);
                         $amka = htmlspecialchars($row["amka"]);
                         $doctorid = htmlspecialchars($row["doctorid"]);
+                        $doctor_name = htmlspecialchars($row["firstname"] . " " . $row["lastname"]);
                         $hospitalclinicid = htmlspecialchars($row["hospitalclinicid"]);
+                        $hospital_name = htmlspecialchars($row["hosp_name"]);
                         $visit_date = htmlspecialchars($row["visit_date"]);
                         $visit_type = htmlspecialchars($row["visit_type"]);
                         echo "<tr>"; 
-                        echo "<td>$visitid</td><td>$amka</td><td>$doctorid</td><td>$hospitalclinicid</td><td>$visit_date</td><td>$visit_type</td>";
+                        echo "<td>$visitid</td><td>$amka</td><td>$doctorid</td><td>$doctor_name</td><td>$hospitalclinicid</td><td>$hospital_name</td><td>$visit_date</td><td>$visit_type</td>";
                         echo "<td><a href='editForm.php?visitid=$visitid'>Edit</a></td>";
                         echo "<td><a href='deleteVisit.php?visitid=$visitid'>Delete</a></td>";
                         echo "</tr>";
                     }
                 } else {
-                    echo "<tr><td colspan='7'>No visits found.</td></tr>";
+                    echo "<tr><td colspan='10'>No visits found.</td></tr>";
                 }
 
                 $conn->close();
@@ -124,10 +132,10 @@
         </div>
 
         <br><br>
-        <a href="../informantion.html">Return to Home Page</a>
+        <a href="../information.html">Return to Home Page</a>
 
         <div class="footer">
-            <p>&copy; 2024 Group 33 (Christos-Spyridon Karydis / Dimitrios Konispoliatis). All rights reserved.</p>
+            <p>&copy; 2024 Group 33 (C. S. Karydis / D. Konispoliatis / A. Georgakopoulos). All rights reserved.</p>
         </div>
     </div>
 </body>
